661ceb5f59e47e6e5717c3d54a887827ae5a3097
Peter Moon <pomoon@gmail.com>
2019-03-25 09:07:24 +0100

    add EBCDIC code page 037

--- cobol-parser/src/main/scala/za/co/absa/cobrix/cobol/parser/decoders/BinaryUtils.scala
+++ cobol-parser/src/main/scala/za/co/absa/cobrix/cobol/parser/decoders/BinaryUtils.scala
@@ -122,6 +122,72 @@ object BinaryUtils {
       '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', spc, spc, spc, spc, spc, spc) // 240 - 255
   }
 
+  /** This is the EBCDIC Code Page 37 to ASCII conversion table with non-printable characters mapping
+   *  from https://en.wikipedia.org/wiki/EBCDIC_037 **/
+  lazy val ebcdicCP037ToAsciiNonPrintable: Array[Char] = {
+    val clf = '\r'
+    val ccr = '\n'
+    val spc = ' '
+    val qts = '\''
+    val qtd = '\"'
+    val bsh = '\\'
+
+    val c00 = '\00'
+    val c01 = '\01'
+    val c02 = '\02'
+    val c03 = '\03'
+    val c04 = '\04'
+    val c05 = '\05'
+    val c06 = '\06'
+    val c07 = '\07'
+    val c08 = '\10'
+    val c09 = '\11'
+    val c0b = '\13'
+    val c0c = '\14'
+    val c0d = '\15'
+    val c0e = '\16'
+    val c0f = '\17'
+    val c10 = '\20'
+    val c11 = '\21'
+    val c12 = '\22'
+    val c13 = '\23'
+    val c14 = '\24'
+    val c15 = '\25'
+    val c16 = '\26'
+    val c17 = '\27'
+    val c18 = '\30'
+    val c19 = '\31'
+    val c1a = '\32'
+    val c1b = '\33'
+    val c1c = '\34'
+    val c1d = '\35'
+    val c1e = '\36'
+    val c1f = '\37'
+    val del = '\177'
+    val shy = '\u00ad'
+    val nel = '\u0085'
+    val rsp = '\u00a0'
+    
+    // Non-printable characters map used: http://www.pacsys.com/asciitab.htm
+    Array[Char](
+      c00, c01, c02, c03, spc, c09, spc, del, spc, spc, spc, c0b, c0c, c0d, c0e, c0f, //   0 -  15
+      c10, c11, c12, c13, spc, nel, c08, spc, c18, c19, spc, spc, c1c, c1d, c1e, c1f, //  16 -  31
+      spc, spc, spc, spc, spc, clf, c17, c1b, spc, spc, spc, spc, spc, c05, c06, c07, //  32 -  47
+      spc, spc, c16, spc, spc, spc, spc, c04, spc, spc, spc, spc, c14, c15, spc, c1a, //  48 -  63
+      ' ', rsp, 'â', 'ä', 'à', 'á', 'ã', 'å', 'ç', 'ñ', '¢', '.', '<', '(', '+', '|', //  64 -  79
+      '&', 'é', 'ê', 'ë', 'è', 'í', 'î', 'ï', 'ì', 'ß', '!', '$', '*', ')', ';', '¬', //  80 -  95
+      '-', '/', 'Â', 'Ä', 'À', 'Á', 'Ã', 'Å', 'Ç', 'Ñ', '|', ',', '%', '_', '>', '?', //  96 - 111
+      'ø', 'É', 'Ê', 'Ë', 'È', 'Í', 'Î', 'Ï', 'Ì', '`', ':', '#', '@', qts, '=', qtd, // 112 - 127
+      'Ø', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', '«', '»', 'ð', 'ý', 'þ', '±', // 128 - 143
+      '°', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 'ª', 'º', 'æ', '¸', 'Æ', '¤', // 144 - 159
+      'µ', '~', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '¡', '¿', 'Ð', 'Ý', 'Þ', '®', // 160 - 175
+      '^', '£', '¥', '·', '©', '§', '¶', '¼', '½', '¾', '[', ']', '¯', '¨', '´', '×', // 176 - 191
+      '{', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', shy, 'ô', 'ö', 'ò', 'ó', 'õ', // 192 - 207
+      '}', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', '¹', 'û', 'ü', 'ù', 'ú', 'ÿ', // 208 - 223
+      bsh, '÷', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '²', 'Ô', 'Ö', 'Ò', 'Ó', 'Õ', // 224 - 239
+      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '³', 'Û', 'Ü', 'Ù', 'Ú', spc) // 240 - 255
+  }
+
   lazy val ascii2ebcdic: Array[Byte] = Array[Byte] (
     0x40.toByte, 0x40.toByte, 0x40.toByte, 0x40.toByte, 0x40.toByte, 0x40.toByte, 0x40.toByte, 0x40.toByte,  //   0 -   7
     0x40.toByte, 0x40.toByte, 0x0d.toByte, 0x40.toByte, 0x40.toByte, 0x25.toByte, 0x40.toByte, 0x40.toByte,  //   8 -  15
