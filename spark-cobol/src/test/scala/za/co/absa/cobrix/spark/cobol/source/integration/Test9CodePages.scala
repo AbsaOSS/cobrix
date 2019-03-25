0d43c223b126269559896ecfe0195dd83245170d
Ruslan Iushchenko (PRG) <Ruslan.Iushchenko@absa.co.za>
2019-03-25 14:48:18 +0100

    Add unit tests for CP037 code page

--- spark-cobol/src/test/scala/za/co/absa/cobrix/spark/cobol/source/integration/Test9CodePages.scala
+++ spark-cobol/src/test/scala/za/co/absa/cobrix/spark/cobol/source/integration/Test9CodePages.scala
@@ -16,9 +16,6 @@
 
 package za.co.absa.cobrix.spark.cobol.source.integration
 
-import java.nio.charset.StandardCharsets
-import java.nio.file.{Files, Paths}
-
 import org.scalatest.FunSuite
 import za.co.absa.cobrix.spark.cobol.source.base.SparkTestBase
 import za.co.absa.cobrix.spark.cobol.utils.FileUtils
@@ -26,18 +23,18 @@ import za.co.absa.cobrix.spark.cobol.utils.FileUtils
 // This test suite will be deprecated soon since search reader is deprecated
 
 //noinspection NameBooleanParameters
-class Test8NonPrintables extends FunSuite with SparkTestBase {
+class Test9CodePages extends FunSuite with SparkTestBase {
 
-  private val exampleName = "Test8"
-  private val inputCopybookPath = "file://../data/test8_copybook.cob"
-  private val inputDataPath = "../data/test8_data"
+  private val exampleName = "Test9"
+  private val inputCopybookPath = "file://../data/test9_copybook.cob"
+  private val inputDataPath = "../data/test9_data"
 
-  private val expectedSchemaPath = "../data/test8_expected/test8_schema.json"
-  private val actualSchemaPath = "../data/test8_expected/test8_schema_actual.json"
+  private val expectedSchemaPath = "../data/test9_expected/test9_schema.json"
+  private val actualSchemaPath = "../data/test9_expected/test9_schema_actual.json"
 
   def runTest(expectPrefix: String, options: Seq[(String, String)]): Unit = {
-    val expectedResultsPath = s"../data/test8_expected/test8$expectPrefix.txt"
-    val actualResultsPath = s"../data/test8_expected/test8${expectPrefix}_actual.txt"
+    val expectedResultsPath = s"../data/test9_expected/test9$expectPrefix.txt"
+    val actualResultsPath = s"../data/test9_expected/test9${expectPrefix}_actual.txt"
 
     val df = {
       val loadDf = spark
@@ -53,32 +50,32 @@ class Test8NonPrintables extends FunSuite with SparkTestBase {
       loadDfWithOptions.load(inputDataPath)
     }
 
-    val expectedSchema = Files.readAllLines(Paths.get(expectedSchemaPath), StandardCharsets.ISO_8859_1).toArray.mkString("\n")
+    val expectedSchema = FileUtils.readAllFileStringUtf8(expectedSchemaPath)
     val actualSchema = df.schema.json
 
     if (actualSchema != expectedSchema) {
-      FileUtils.writeStringToFile(actualSchema, actualSchemaPath)
+      FileUtils.writeStringToUtf8File(actualSchema, actualSchemaPath)
       assert(false, s"The actual schema doesn't match what is expected for $exampleName example. Please compare contents of $expectedSchemaPath to " +
         s"$actualSchemaPath for details.")
     }
 
     val actual = df.toJSON.take(60)
-    val expected = Files.readAllLines(Paths.get(expectedResultsPath), StandardCharsets.ISO_8859_1).toArray
+    val expected = FileUtils.readAllFileLinesUtf8(expectedResultsPath)
 
     if (!actual.sameElements(expected)) {
-      FileUtils.writeStringsToFile(actual, actualResultsPath)
+      FileUtils.writeStringsToUtf8File(actual, actualResultsPath)
       assert(false, s"The actual data doesn't match what is expected for $exampleName example. Please compare contents of $expectedResultsPath to $actualResultsPath for details.")
     }
   }
 
-  test(s"Integration test on $exampleName (printable characters) data") {
-    runTest("_printable",
-      "ebcdic_code_page" -> "common" :: Nil)
+  test(s"Integration test on $exampleName code page CP037") {
+    runTest("_cp037",
+      "ebcdic_code_page" -> "cp037" :: Nil)
   }
 
-  test(s"Integration test on $exampleName (non-printable characters) data") {
-    runTest("_non_printable",
-      "ebcdic_code_page" -> "common_extended" ::
+  test(s"Integration test on $exampleName code page CP037 extended") {
+    runTest("_cp037_ext",
+      "ebcdic_code_page" -> "cp037_extended" ::
         "string_trimming_policy" -> "none" :: Nil)
   }
 
