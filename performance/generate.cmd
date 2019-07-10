c34ff024fe457f28d6651ff821fb91cc0414d112
Ruslan Iushchenko <yruslan@gmail.com>
2019-07-10 16:30:11 +0200

    Cleanup POMs, add mandatory license check when releasing public.

--- performance/generate.cmd
+++ performance/generate.cmd
@@ -1,3 +1,16 @@
+REM Copyright 2018-2019 ABSA Group Limited
+REM
+REM Licensed under the Apache License, Version 2.0 (the "License");
+REM you may not use this file except in compliance with the License.
+REM You may obtain a copy of the License at
+REM     http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing, software
+REM distributed under the License is distributed on an "AS IS" BASIS,
+REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM See the License for the specific language governing permissions and
+REM limitations under the License.
+
 d:\Tools\gnuplot\bin\gnuplot.exe -c exp2_multiseg_narrow.plot
 d:\Tools\gnuplot\bin\gnuplot.exe -c exp3_multiseg_wide.plot
 d:\Tools\gnuplot\bin\gnuplot.exe -c exp1_raw_records.plot
