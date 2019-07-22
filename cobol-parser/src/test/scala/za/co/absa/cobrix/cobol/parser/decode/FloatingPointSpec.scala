27dd67b29ec2a62258cd4162fd2ab68e1cf3c960
Ruslan Iushchenko <yruslan@gmail.com>
2019-07-22 14:51:59 +0200

    #128 Add support for IBM floating point formats

--- cobol-parser/src/test/scala/za/co/absa/cobrix/cobol/parser/decode/FloatingPointSpec.scala
+++ cobol-parser/src/test/scala/za/co/absa/cobrix/cobol/parser/decode/FloatingPointSpec.scala
@@ -21,52 +21,62 @@ import za.co.absa.cobrix.cobol.parser.decoders.FloatingPointDecoders
 
 class FloatingPointSpec extends FunSuite {
 
-  def floatEqual(a: Float, b: Float): Boolean = {
-    Math.abs(a - b) < 0.00001
+  def assertFloatEqual(a: Float, b: Float): Unit = {
+    assert(Math.abs(a - b) < 0.00001, s"($a != $b)")
   }
 
-  def doubleEqual(a: Double, b: Double): Boolean = {
-    Math.abs(a - b) < 0.0000000001
+  def assertDoubleEqual(a: Double, b: Double): Unit = {
+    assert(Math.abs(a - b) < 0.0000000001, s"($a != $b)")
   }
 
   test("Test decoding IBM single precision / big-endian FP numbers decoding") {
-
+    val bytes = Array[Byte](
+      0x43.toByte, 0x14.toByte, 0x2E.toByte, 0xFC.toByte)
+    assertFloatEqual(FloatingPointDecoders.decodeIbmSingleBigEndian(bytes), 5.045883f)
   }
 
   test("Test decoding IBM double precision / big-endian FP numbers decoding") {
-
+    val bytes = Array[Byte](
+      0x43.toByte, 0x14.toByte, 0x2E.toByte, 0xFC.toByte,
+      0xCA.toByte, 0xF7.toByte, 0x09.toByte, 0xB7.toByte)
+    assertDoubleEqual(FloatingPointDecoders.decodeIbmDoubleBigEndian(bytes), 322.936717)
   }
 
   test("Test decoding IBM single precision / little-endian FP numbers decoding ") {
-
+    val bytes = Array[Byte](
+      0xFC.toByte, 0x2E.toByte, 0x14.toByte, 0x43.toByte)
+    assertFloatEqual(FloatingPointDecoders.decodeIbmSingleLittleEndian(bytes), 5.045883f)
   }
 
   test("Test decoding IBM double precision / little-endian FP numbers decoding") {
-
+    val bytes = Array[Byte](
+      0xB7.toByte, 0x09.toByte, 0xF7.toByte, 0xCA.toByte,
+      0xFC.toByte, 0x2E.toByte, 0x14.toByte, 0x43.toByte)
+    assertDoubleEqual(FloatingPointDecoders.decodeIbmDoubleLittleEndian(bytes), 322.936717)
   }
 
   test("Test decoding IEEE754 single precision / big-endian FP numbers decoding") {
     val bytes = Array[Byte](0x40.toByte, 0x49.toByte, 0x0F.toByte, 0xDA.toByte)
-    assert(floatEqual(FloatingPointDecoders.decodeIeee754SingleBigEndian(bytes), 3.1415925f))
+    assertFloatEqual(FloatingPointDecoders.decodeIeee754SingleBigEndian(bytes), 3.1415925f)
   }
 
   test("Test decoding IEEE754 double precision / big-endian FP numbers decoding") {
     val bytes = Array[Byte](
       0x40.toByte, 0x09.toByte, 0x21.toByte, 0xFB.toByte,
       0x54.toByte, 0x44.toByte, 0x2E.toByte, 0xEA.toByte)
-    assert(doubleEqual(FloatingPointDecoders.decodeIeee754DoubleBigEndian(bytes), 3.14159265359))
+    assertDoubleEqual(FloatingPointDecoders.decodeIeee754DoubleBigEndian(bytes), 3.14159265359)
   }
 
   test("Test decoding IEEE754 single precision / little-endian FP numbers decoding ") {
     val bytes = Array[Byte](0xDA.toByte, 0x0F.toByte, 0x49.toByte, 0x40.toByte)
-    assert(floatEqual(FloatingPointDecoders.decodeIeee754SingleLittleEndian(bytes), 3.1415925f))
+    assertFloatEqual(FloatingPointDecoders.decodeIeee754SingleLittleEndian(bytes), 3.1415925f)
   }
 
   test("Test decoding IEEE754 double precision / little-endian FP numbers decoding") {
     val bytes = Array[Byte](
       0xEA.toByte, 0x2E.toByte, 0x44.toByte, 0x54.toByte,
       0xFB.toByte, 0x21.toByte, 0x09.toByte, 0x40.toByte)
-    assert(doubleEqual(FloatingPointDecoders.decodeIeee754DoubleLittleEndian(bytes), 3.14159265359))
+    assertDoubleEqual(FloatingPointDecoders.decodeIeee754DoubleLittleEndian(bytes), 3.14159265359)
   }
 
 }
