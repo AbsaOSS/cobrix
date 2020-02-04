c6a4f1d75e3d3909839a3067278426ce54d0f10f
Ruslan Yushchenko <yruslan@gmail.com>
2020-02-04 21:28:57 +0100

    #240 Add decoder support for comma as a separator.

---
+++ spark-cobol/src/test/scala/za/co/absa/cobrix/spark/cobol/source/regression/Test05CommaDecimals.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2018 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.cobrix.spark.cobol.source.regression
+
+import org.scalatest.FunSuite
+import org.slf4j.LoggerFactory
+import za.co.absa.cobrix.spark.cobol.source.base.SparkTestBase
+import za.co.absa.cobrix.spark.cobol.source.fixtures.BinaryFileFixture
+
+class Test05CommaDecimals extends FunSuite with SparkTestBase with BinaryFileFixture {
+
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
+  private val copybook =
+    """       01  R.
+                03 N     PIC +999,99 USAGE DISPLAY.
+    """
+
+  val binFileContents: Array[Byte] = Array[Byte](
+    // +112,34
+    0x4E.toByte, 0xF1.toByte, 0xF1.toByte, 0xF2.toByte, 0x6B.toByte, 0xF3.toByte, 0xF4.toByte,
+    //  -23,45
+    0x40.toByte, 0x60.toByte, 0xF2.toByte, 0xF3.toByte, 0x6B.toByte, 0xF4.toByte, 0xF5.toByte,
+    // +005,00
+    0x4E.toByte, 0xF0.toByte, 0xF0.toByte, 0xF5.toByte, 0x6B.toByte, 0xF0.toByte, 0xF0.toByte
+  )
+
+  test("Test input data file having a numeric field with a commaa as the decimal separator") {
+    withTempBinFile("binary", ".dat", binFileContents) { tmpFileName =>
+      val df = spark
+        .read
+        .format("cobol")
+        .option("copybook_contents", copybook)
+        .option("schema_retention_policy", "collapse_root")
+        .load(tmpFileName)
+
+      val expected = """[{"N":112.34},{"N":-23.45},{"N":5.00}]"""
+
+      val actual = df.toJSON.collect().mkString("[", ",", "]")
+
+      assertResults(actual, expected)
+    }
+
+  }
+
+  def assertResults(actualResults: String, expectedResults: String): Unit = {
+    if (actualResults != expectedResults) {
+      logger.error(s"EXPECTED:\n$expectedResults")
+      logger.error(s"ACTUAL:\n$actualResults")
+      fail("Actual dataset data does not match the expected data (see above).")
+    }
+  }
+
+}
