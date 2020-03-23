acf825c212c502289750ccf4bfde53380de66677
Ruslan Yushchenko <yruslan@gmail.com>
2020-03-23 18:03:59 +0100

    #224 All examples are moved out of production code into a separate project.

---
+++ examples/examples-collection/src/main/scala/com/example/spark/cobol/examples/apps/CobolSparkExample2.scala
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2018 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.spark.cobol.examples.apps
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.StringType
+import org.apache.log4j.{Level, Logger}
+
+// This is an example Spark Job that uses COBOL data source.
+// IMPORTANT! To run this locally change the scope of all Scala and Spark libraries from 'provided' to 'compile' in pom.xml
+//            But revert it to 'provided' to create an uber jar for running on a cluster
+object CobolSparkExample2 {
+
+  def main(args: Array[String]): Unit = {
+
+    // Making the logs less verbose for this example
+    Logger.getLogger("org").setLevel(Level.WARN)
+    Logger.getLogger("akka").setLevel(Level.WARN)
+
+    val sparkBuilder = SparkSession.builder().appName("Cobol source reader example 2")
+    val spark = sparkBuilder
+      .master("local[*]")
+      .getOrCreate()
+
+    val copybook =
+      """        01  COMPANY-DETAILS.
+        |            05  SEGMENT-ID           PIC X(5).
+        |            05  COMPANY-ID           PIC X(10).
+        |            05  COMPANY.
+        |               10  NAME              PIC X(15).
+        |               10  ADDRESS           PIC X(25).
+        |               10  TAXPAYER.
+        |                  15  TAXPAYER-TYPE  PIC X(1).
+        |                  15  TAXPAYER-STR   PIC X(8).
+        |                  15  TAXPAYER-NUM  REDEFINES TAXPAYER-STR
+        |                                     PIC 9(8) COMP.
+        |            05  CONTACT REDEFINES COMPANY.
+        |               10  PHONE-NUMBER      PIC X(17).
+        |               10  CONTACT-PERSON    PIC X(28).
+        |""".stripMargin
+
+    // This is an example read a multisegment variable length file from a mainframe.
+    val df = spark
+      .read
+      .format("cobol")
+      .option("copybook_contents", copybook)
+      .option("schema_retention_policy", "collapse_root")     // Collapses the root group returning it's field on the top level of the schema
+      .option("is_record_sequence", "true")                   // Specifies that the input file is a sequence of records having RDW headers
+      .load("../../examples/example_data/multisegment_data/COMP.DETAILS.SEP30.DATA.dat")
+
+    import spark.implicits._
+
+    df.printSchema
+    //println(df.count)
+    df.orderBy($"COMPANY_ID").show(10, false)
+
+    val dfCompanies = df.filter($"SEGMENT_ID"==="C")
+      .select($"COMPANY_ID", $"COMPANY.NAME".as("COMPANY_NAME"), $"COMPANY.ADDRESS",
+        when($"COMPANY.TAXPAYER.TAXPAYER_TYPE" === "A", $"COMPANY.TAXPAYER.TAXPAYER_STR")
+          .otherwise($"COMPANY.TAXPAYER.TAXPAYER_NUM").cast(StringType).as("TAXPAYER"))
+
+    dfCompanies.printSchema
+    //println(df.count)
+    dfCompanies.show(50, truncate = false)
+
+    val dfContacts = df.filter($"SEGMENT_ID"==="P")
+      .select($"COMPANY_ID", $"CONTACT.CONTACT_PERSON", $"CONTACT.PHONE_NUMBER")
+
+    dfContacts.printSchema
+    //println(df.count)
+    dfContacts.show(50, truncate = false)
+
+    val dfJoined = dfCompanies.join(dfContacts, "COMPANY_ID")
+
+    dfJoined.printSchema
+    //println(df.count)
+    dfJoined.orderBy($"COMPANY_ID").show(50, truncate = false)
+  }
+
+}
---
+++ examples/examples-collection/src/main/scala/com/example/spark/cobol/examples/parser/generators/TestDataGen8NonPrintableNames.scala
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2018 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.spark.cobol.examples.parser.generators
+
+import java.io.{BufferedOutputStream, FileOutputStream}
+import za.co.absa.cobrix.cobol.parser.examples.generators.model.{CommonLists, Company}
+import za.co.absa.cobrix.cobol.parser.examples.generators.utils.GeneratorTools._
+
+import scala.util.Random
+
+/**
+  * This is a test data generator. The copybook for it is listed below.
+  */
+object TestDataGen8NonPrintableNames {
+
+  val numberOfRecordsToGenerate = 1000
+
+  // seed=100 is used for the integration test
+  val rand: Random = new Random(100)
+
+  /*
+          01  TRANSDATA.
+              05  CURRENCY          PIC X(3).
+              05  SIGNATURE         PIC X(8).
+              05  COMPANY-NAME-NP   PIC X(15).
+              05  COMPIANY-ID       PIC X(10).
+              05  WEALTH-QFY        PIC 9(1).
+              05  AMOUNT            PIC S9(09)V99  BINARY.
+   */
+
+  val currencies: Seq[String] = CommonLists.currencies
+
+  val companies: Seq[Company] = CommonLists.companiesWithNonPrintableCharacters
+
+  def main(args: Array[String]): Unit = {
+
+    val numOfCurrencies = currencies.size
+    val numOfCompanies = companies.size
+
+    val byteArray: Array[Byte] = new Array[Byte](45)
+    val bos = new BufferedOutputStream(new FileOutputStream("TRAN2.MAR14.DATA.dat"))
+    var i = 0
+    val sig = "S9276511"
+    while (i< numberOfRecordsToGenerate) {
+      val currency = currencies(rand.nextInt(numOfCurrencies))
+      val company = companies(rand.nextInt(numOfCompanies))
+
+      putStringToArray(byteArray, currency, 0, 2) // 3
+      putStringToArray(byteArray, sig, 3, 10)  // 8
+
+      putEbcdicStringToArray(byteArray, company.companyName, 11, 25) //15
+      putStringToArray(byteArray, company.companyId, 26, 35) //10
+
+      val tp = rand.nextInt(100)
+      val amountIntPart = if (tp<80){
+        rand.nextInt(1000).toLong
+      } else if (tp<95) {
+        rand.nextInt(100000).toLong
+      } else {
+        rand.nextInt(10000000).toLong
+      }
+
+      val amountFracPart = if (amountIntPart < 10000) rand.nextInt(100) else 0
+
+      putDecimalToArray(byteArray, amountIntPart, amountFracPart, 37, 44) // 8
+
+      if ( rand.nextInt(100) < 37 )
+        putStringToArray(byteArray, "1", 36, 36) // 1
+      else
+        putStringToArray(byteArray, "0", 36, 36)
+
+      bos.write(byteArray)
+      i += 1
+    }
+    bos.close()
+  }
+}
