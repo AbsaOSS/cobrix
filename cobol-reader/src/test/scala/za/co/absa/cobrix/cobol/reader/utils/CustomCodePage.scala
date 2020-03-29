e204540c843fc40596cf7b862e3f2ac9cecda2bd
Tiago Requeijo <tiago.requeijo.dev@gmail.com>
2020-03-28 21:14:12 -0400

    Moved tests

--- cobol-reader/src/test/scala/za/co/absa/cobrix/cobol/reader/utils/CustomCodePage.scala
+++ cobol-reader/src/test/scala/za/co/absa/cobrix/cobol/reader/utils/CustomCodePage.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package za.co.absa.cobrix.spark.cobol.source.utils
+package za.co.absa.cobrix.cobol.reader.utils
 
 import za.co.absa.cobrix.cobol.parser.encoding.codepage.CodePage
 
