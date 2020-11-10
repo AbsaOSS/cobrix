bc81e7e57756430d6454c4f47187fd98adb93149
Ruslan Iushchenko <yruslan@gmail.com>
2020-11-10 09:02:38 +0100

    Add a unit test to ensure the layout of debugging fields is correct for chains of redefines.

--- cobol-parser/src/test/scala/za/co/absa/cobrix/cobol/parser/copybooks/ParseDebugRedefinedSpec.scala
+++ cobol-parser/src/test/scala/za/co/absa/cobrix/cobol/parser/copybooks/ParseDebugRedefinedSpec.scala
@@ -27,23 +27,61 @@ class ParseDebugRedefinedSpec extends FunSuite {
   private val logger = LoggerFactory.getLogger(this.getClass)
 
   test("Test copybook parser generates debug columns for redefined fields") {
-    val copybookWithRedefined: String = """        01  TRANSDATA.
-                                          |           10 CURRENCY          PIC X(3).
-                                          |           10 field1 PIC X(2).
-                                          |           10 field2 redefines field1 PIC X.
-                                          |""".stripMargin.replace("\r\n", "\n")
+    val copybookWithRedefined: String =
+      """        01  TRANSDATA.
+        |           10 CURRENCY                PIC X(3).
+        |           10 FIELD1                  PIC X(2).
+        |           10 FIELD2 REDEFINES FIELD1 PIC X.
+        |""".stripMargin.replace("\r\n", "\n")
 
     val copybook = CopybookParser.parseTree(copybookWithRedefined, debugFieldsPolicy = HexValue)
-    val attributes: Set[Statement] = copybook.ast.children(0).asInstanceOf[Group].children.toSet
-    val expectedAttributes: Set[String] = Set("CURRENCY","CURRENCY_debug",
-                                         "field1", "field1_debug",
-                                         "field2","field2_debug")
-    var actualAttributes: Set[String] = Set()
-    attributes.foreach(i => {
-      actualAttributes += i.name
+    val fields: Set[Statement] = copybook.ast.children(0).asInstanceOf[Group].children.toSet
+    val expectedFields: Set[String] =
+      Set("CURRENCY", "CURRENCY_debug",
+        "FIELD1", "FIELD1_debug",
+        "FIELD2", "FIELD2_debug")
+
+    var actualFields: Set[String] = Set()
+    fields.foreach(i => {
+      actualFields += i.name
     })
 
-    assert(attributes.size == 6)
-    assert(expectedAttributes.equals(actualAttributes))
+    assert(fields.size == 6)
+    assert(expectedFields.equals(actualFields))
+  }
+
+  test("Test redefined and redefines flags are retained when adding debugging fields") {
+    val copybookWithRedefined: String =
+      """        01  TRANSDATA.
+        |           10 CURRENCY                PIC X(3).
+        |           10 FIELD1                  PIC X(2).
+        |           10 FIELD2 REDEFINES FIELD1 PIC X.
+        |           10 FIELD3 REDEFINES FIELD1 PIC X(4).
+        |           10 FIELD4 REDEFINES FIELD2 PIC XX.
+        |                                          |"""
+        .stripMargin.replace("\r\n", "\n")
+
+    val expectedLayout =
+      """-------- FIELD LEVEL/NAME --------- --ATTRIBS--    FLD  START     END  LENGTH
+        |
+        |TRANSDATA                                                    1      7      7
+        |  10 CURRENCY                          r              1      1      3      3
+        |  10 CURRENCY_debug                    R              2      1      3      3
+        |  10 FIELD1                            r              3      4      7      4
+        |  10 FIELD1_debug                      rR             4      4      7      4
+        |  10 FIELD2                            rR             5      4      7      4
+        |  10 FIELD2_debug                      rR             6      4      7      4
+        |  10 FIELD3                            rR             7      4      7      4
+        |  10 FIELD3_debug                      rR             8      4      7      4
+        |  10 FIELD4                            rR             9      4      7      4
+        |  10 FIELD4_debug                      R             10      4      7      4"""
+        .stripMargin.replace("\r\n", "\n")
+
+    val copybook = CopybookParser.parseTree(copybookWithRedefined, debugFieldsPolicy = HexValue)
+
+
+    val actualLayout = copybook.generateRecordLayoutPositions()
+
+    assert(actualLayout == expectedLayout)
   }
 }
