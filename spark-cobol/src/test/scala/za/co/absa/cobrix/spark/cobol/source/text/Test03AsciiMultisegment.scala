af519907c5cdfbe3a8d58805b74c6eaad8f1a03a
Ruslan Iushchenko <yruslan@gmail.com>
2020-11-20 13:45:58 +0100

    Use WordSpec for new unit tests.

--- spark-cobol/src/test/scala/za/co/absa/cobrix/spark/cobol/source/text/Test03AsciiMultisegment.scala
+++ spark-cobol/src/test/scala/za/co/absa/cobrix/spark/cobol/source/text/Test03AsciiMultisegment.scala
@@ -18,12 +18,12 @@ package za.co.absa.cobrix.spark.cobol.source.text
 
 import java.nio.charset.StandardCharsets
 
-import org.scalatest.FunSuite
+import org.scalatest.WordSpec
 import org.slf4j.{Logger, LoggerFactory}
 import za.co.absa.cobrix.spark.cobol.source.base.{SimpleComparisonBase, SparkTestBase}
 import za.co.absa.cobrix.spark.cobol.source.fixtures.BinaryFileFixture
 
-class Test03AsciiMultisegment extends FunSuite with SparkTestBase with BinaryFileFixture with SimpleComparisonBase {
+class Test03AsciiMultisegment extends WordSpec with SparkTestBase with BinaryFileFixture with SimpleComparisonBase {
   private implicit val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
   private val copybook =
@@ -37,33 +37,37 @@ class Test03AsciiMultisegment extends FunSuite with SparkTestBase with BinaryFil
              10  B2       PIC X(5).
     """
 
-  test("Test multisegment records parsing") {
-    val textFileContent: String =
-      Seq("1Tes  0123456789",
-        "2Test 012345",
-        "1None Data¡3    ",
-        "2 on  Data 4").mkString("\n")
+  ""
 
-    withTempTextFile("text_ascii", ".txt", StandardCharsets.UTF_8, textFileContent) { tmpFileName =>
-      val df = spark
-        .read
-        .format("cobol")
-        .option("copybook_contents", copybook)
-        .option("pedantic", "true")
-        .option("is_text", "true")
-        .option("encoding", "ascii")
-        .option("is_record_sequence", "true")
-        .option("schema_retention_policy", "collapse_root")
-        .option("segment_field", "T")
-        .option("redefine-segment-id-map:00", "R1 => 1")
-        .option("redefine-segment-id-map:01", "R2 => 2")
-        .load(tmpFileName)
+  "Multisegment ASCII files should be parsed" when {
+    "Records perfectly match the schema" in {
+      val textFileContent: String =
+        Seq("1Tes  0123456789",
+          "2Test 012345",
+          "1None Data¡3    ",
+          "2 on  Data 4").mkString("\n")
 
-      val expected = """[{"T":"1","R1":{"A2":"Tes","A3":"0123456789"}},{"T":"2","R2":{"B1":"Test","B2":"01234"}},{"T":"1","R1":{"A2":"None","A3":"Data  3"}},{"T":""}]"""
+      withTempTextFile("text_ascii", ".txt", StandardCharsets.UTF_8, textFileContent) { tmpFileName =>
+        val df = spark
+          .read
+          .format("cobol")
+          .option("copybook_contents", copybook)
+          .option("pedantic", "true")
+          .option("is_text", "true")
+          .option("encoding", "ascii")
+          .option("is_record_sequence", "true")
+          .option("schema_retention_policy", "collapse_root")
+          .option("segment_field", "T")
+          .option("redefine-segment-id-map:00", "R1 => 1")
+          .option("redefine-segment-id-map:01", "R2 => 2")
+          .load(tmpFileName)
 
-      val actual = df.toJSON.collect().mkString("[", ",", "]")
+        val expected = """[{"T":"1","R1":{"A2":"Tes","A3":"0123456789"}},{"T":"2","R2":{"B1":"Test","B2":"01234"}},{"T":"1","R1":{"A2":"None","A3":"Data  3"}},{"T":""}]"""
 
-      assertEqualsMultiline(actual, expected)
+        val actual = df.toJSON.collect().mkString("[", ",", "]")
+
+        assertEqualsMultiline(actual, expected)
+      }
     }
   }
 
