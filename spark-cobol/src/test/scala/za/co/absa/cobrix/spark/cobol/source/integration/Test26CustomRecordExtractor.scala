bf41a5bd77632f8caaed58e55366a8cb47ef5be1
Ruslan Iushchenko <yruslan@gmail.com>
2020-12-02 09:56:54 +0100

    #338 Do not accept options incompatible with custom record extractors.

--- spark-cobol/src/test/scala/za/co/absa/cobrix/spark/cobol/source/integration/Test26CustomRecordExtractor.scala
+++ spark-cobol/src/test/scala/za/co/absa/cobrix/spark/cobol/source/integration/Test26CustomRecordExtractor.scala
@@ -16,13 +16,14 @@
 
 package za.co.absa.cobrix.spark.cobol.source.integration
 
-import org.scalatest.FunSuite
+import org.apache.spark.sql.DataFrame
+import org.scalatest.WordSpec
 import za.co.absa.cobrix.spark.cobol.mocks.CustomRecordExtractorMock
 import za.co.absa.cobrix.spark.cobol.source.base.SparkTestBase
 import za.co.absa.cobrix.spark.cobol.source.fixtures.BinaryFileFixture
 
 //noinspection NameBooleanParameters
-class Test26CustomRecordExtractor extends FunSuite with SparkTestBase with BinaryFileFixture {
+class Test26CustomRecordExtractor extends WordSpec with SparkTestBase with BinaryFileFixture {
 
   private val exampleName = "Test26 (custom record extractor)"
 
@@ -32,25 +33,75 @@ class Test26CustomRecordExtractor extends FunSuite with SparkTestBase with Binar
       """
   private val data = "AABBBCCDDDEEFFF"
 
-  test(s"Integration test on $exampleName - custom record extractor") {
-    val expected = """[{"A":"AA"},{"A":"BBB"},{"A":"CC"},{"A":"DDD"},{"A":"EE"},{"A":"FFF"}]"""
+  "Custom record extractor" should {
+    "apply the extractor to a binary data" in {
+        val expected = """[{"A":"AA"},{"A":"BBB"},{"A":"CC"},{"A":"DDD"},{"A":"EE"},{"A":"FFF"}]"""
 
-    withTempBinFile("custom_re", ".dat", data.getBytes) { tmpFileName =>
-      val df = spark
-        .read
-        .format("cobol")
-        .option("copybook_contents", copybook)
-        //.option("is_record_sequence", "true")
-        .option("encoding", "ascii")
-        .option("schema_retention_policy", "collapse_root")
-        .option("record_extractor", "za.co.absa.cobrix.spark.cobol.mocks.CustomRecordExtractorMock")
-        .option("re_additional_info", "re info")
-        .load(tmpFileName)
+        withTempBinFile("custom_re", ".dat", data.getBytes) { tmpFileName =>
+          val df = getDataFrame(tmpFileName)
 
-      val actual = df.toJSON.collect().mkString("[", ",", "]")
+          val actual = df.toJSON.collect().mkString("[", ",", "]")
 
-      assert(actual == expected)
-      assert(CustomRecordExtractorMock.additionalInfo == "re info")
+          assert(actual == expected)
+          assert(CustomRecordExtractorMock.additionalInfo == "re info")
+        }
+      }
+    }
+
+  "Custom record extractor options are not compatible with" when {
+    "is_record_sequence" in {
+      intercept[IllegalArgumentException] {
+        getDataFrame("/dummy", Map("is_record_sequence" -> "true"))
+      }
+    }
+    "is_xcom" in {
+      intercept[IllegalArgumentException] {
+        getDataFrame("/dummy", Map("is_xcom" -> "true"))
+      }
+    }
+    "is_rdw_big_endian" in {
+      intercept[IllegalArgumentException] {
+        getDataFrame("/dummy", Map("is_rdw_big_endian" -> "true"))
+      }
+    }
+    "is_rdw_part_of_record_length" in {
+      intercept[IllegalArgumentException] {
+        getDataFrame("/dummy", Map("is_rdw_part_of_record_length" -> "true"))
+      }
+    }
+    "rdw_adjustment" in {
+      intercept[IllegalArgumentException] {
+        getDataFrame("/dummy", Map("rdw_adjustment" -> "-1"))
+      }
+    }
+    "record_length_field" in {
+      intercept[IllegalArgumentException] {
+        getDataFrame("/dummy", Map("record_length_field" -> "A"))
+      }
+    }
+    "record_header_parser" in {
+      intercept[IllegalArgumentException] {
+        getDataFrame("/dummy", Map("record_header_parser" -> "com.example.parser"))
+      }
+    }
+    "rhp_additional_info" in {
+      intercept[IllegalArgumentException] {
+        getDataFrame("/dummy", Map("rhp_additional_info" -> "Additional info"))
+      }
     }
   }
+
+  private def getDataFrame(inputPath: String, extraOptions: Map[String, String] = Map.empty[String, String]): DataFrame = {
+    spark
+      .read
+      .format("cobol")
+      .option("copybook_contents", copybook)
+      .option("encoding", "ascii")
+      .option("schema_retention_policy", "collapse_root")
+      .option("record_extractor", "za.co.absa.cobrix.spark.cobol.mocks.CustomRecordExtractorMock")
+      .option("re_additional_info", "re info")
+      .options(extraOptions)
+      .load(inputPath)
+  }
+
 }
