3baed0305f32190b723270a64387a88be9799a85
Ruslan Iushchenko <yruslan@gmail.com>
2022-08-08 13:26:29 +0200

    #510 Fix FILLERs being dropped for a FILLER with OCCURS GROUP.

--- cobol-parser/src/test/scala/za/co/absa/cobrix/cobol/parser/copybooks/ParseCopybookFeaturesSpec.scala
+++ cobol-parser/src/test/scala/za/co/absa/cobrix/cobol/parser/copybooks/ParseCopybookFeaturesSpec.scala
@@ -19,6 +19,7 @@ package za.co.absa.cobrix.cobol.parser.copybooks
 import org.scalatest.FunSuite
 import org.slf4j.{Logger, LoggerFactory}
 import za.co.absa.cobrix.cobol.parser.CopybookParser
+import za.co.absa.cobrix.cobol.parser.ast.Group
 import za.co.absa.cobrix.cobol.testutils.SimpleComparisonBase
 
 class ParseCopybookFeaturesSpec extends FunSuite with SimpleComparisonBase {
@@ -28,6 +29,10 @@ class ParseCopybookFeaturesSpec extends FunSuite with SimpleComparisonBase {
     """        01  RECORD.
       |            05  FILLER           PIC X(1).
       |            05  COMPANY_PREFIX   PIC X(3).
+      |            05  FIELD1.
+      |              07 FILLER OCCURS 12 TIMES.
+      |               10 CHILD1 PIC S9(7) COMP-3.
+      |               10 CHILD2 PIC S99V99999 COMP-3.
       |            05  FILLER           PIC X(1).
       |            05  FILLER           PIC X(1).
       |            05  COMPANY_NAME     PIC X(9).
@@ -122,31 +127,37 @@ class ParseCopybookFeaturesSpec extends FunSuite with SimpleComparisonBase {
     val expectedLayout =
       """-------- FIELD LEVEL/NAME --------- --ATTRIBS--    FLD  START     END  LENGTH
         |
-        |1 RECORD                                              1      1     60     60
+        |1 RECORD                                              1      1    156    156
         |  5 FILLER_P1                                         2      1      1      1
         |  5 COMPANY_PREFIX                                    3      2      4      3
-        |  5 FILLER_P2                                         4      5      5      1
-        |  5 FILLER_P3                                         5      6      6      1
-        |  5 COMPANY_NAME                       r              6      7     15      9
-        |  5 FILLER_1                           R              7      7     15      9
-        |    10 STR1                                           8      7     11      5
-        |    10 STR2                                           9     12     13      2
-        |    10 FILLER_P4                                     10     14     14      1
-        |  5 ADDRESS                            r             11     16     45     30
-        |  5 FILLER_2                           R             12     16     45     30
-        |    10 STR4                                          13     16     25     10
-        |    10 FILLER_P5                                     14     26     45     20
-        |  5 FILL_FIELD                         r             15     46     52      7
-        |    10 FILLER_P6                                     16     46     50      5
-        |    10 FILLER_P7                                     17     51     52      2
-        |  5 CONTACT_PERSON                     R             18     46     52      7
-        |    10 FIRST_NAME                                    19     46     51      6
-        |  5 AMOUNT                                           20     53     60      8
+        |  5 FIELD1                                            4      5    100     96
+        |    7 FILLER_1                         []             5      5    100     96
+        |      10 CHILD1                                       6      5      8      4
+        |      10 CHILD2                                       7      9     12      4
+        |  5 FILLER_P2                                         8    101    101      1
+        |  5 FILLER_P3                                         9    102    102      1
+        |  5 COMPANY_NAME                       r             10    103    111      9
+        |  5 FILLER_2                           R             11    103    111      9
+        |    10 STR1                                          12    103    107      5
+        |    10 STR2                                          13    108    109      2
+        |    10 FILLER_P4                                     14    110    110      1
+        |  5 ADDRESS                            r             15    112    141     30
+        |  5 FILLER_3                           R             16    112    141     30
+        |    10 STR4                                          17    112    121     10
+        |    10 FILLER_P5                                     18    122    141     20
+        |  5 FILL_FIELD                         r             19    142    148      7
+        |    10 FILLER_P6                                     20    142    146      5
+        |    10 FILLER_P7                                     21    147    148      2
+        |  5 CONTACT_PERSON                     R             22    142    148      7
+        |    10 FIRST_NAME                                    23    142    147      6
+        |  5 AMOUNT                                           24    149    156      8
         |"""
         .stripMargin.replace("\r\n", "\n")
 
     assertEqualsMultiline(layout, expectedLayout)
+    assert(!copybook.ast.children(0).asInstanceOf[Group].children(2).isFiller)
   }
+
   test("Test parseSimple() drop value fillers") {
     val copybook = CopybookParser.parseSimple(copybookFillers, dropGroupFillers = false, dropValueFillers = true, dropFillersFromAst = true)
     val layout = copybook.generateRecordLayoutPositions()
@@ -154,22 +165,27 @@ class ParseCopybookFeaturesSpec extends FunSuite with SimpleComparisonBase {
     val expectedLayout =
       """-------- FIELD LEVEL/NAME --------- --ATTRIBS--    FLD  START     END  LENGTH
         |
-        |1 RECORD                                              1      1     60     60
+        |1 RECORD                                              1      1    156    156
         |  5 COMPANY_PREFIX                                    2      2      4      3
-        |  5 COMPANY_NAME                       r              3      7     15      9
-        |  5 FILLER_1                           R              4      7     15      9
-        |    10 STR1                                           5      7     11      5
-        |    10 STR2                                           6     12     13      2
-        |  5 ADDRESS                            r              7     16     45     30
-        |  5 FILLER_2                           R              8     16     45     30
-        |    10 STR4                                           9     16     25     10
-        |  5 CONTACT_PERSON                     R             10     46     52      7
-        |    10 FIRST_NAME                                    11     46     51      6
-        |  5 AMOUNT                                           12     53     60      8
+        |  5 FIELD1                                            3      5    100     96
+        |    7 FILLER_1                         []             4      5    100     96
+        |      10 CHILD1                                       5      5      8      4
+        |      10 CHILD2                                       6      9     12      4
+        |  5 COMPANY_NAME                       r              7    103    111      9
+        |  5 FILLER_2                           R              8    103    111      9
+        |    10 STR1                                           9    103    107      5
+        |    10 STR2                                          10    108    109      2
+        |  5 ADDRESS                            r             11    112    141     30
+        |  5 FILLER_3                           R             12    112    141     30
+        |    10 STR4                                          13    112    121     10
+        |  5 CONTACT_PERSON                     R             14    142    148      7
+        |    10 FIRST_NAME                                    15    142    147      6
+        |  5 AMOUNT                                           16    149    156      8
         |"""
         .stripMargin.replace("\r\n", "\n")
 
     assertEqualsMultiline(layout, expectedLayout)
+    assert(!copybook.ast.children(0).asInstanceOf[Group].children(2).isFiller)
   }
 
   test("Test parseSimple() drop group fillers") {
@@ -179,19 +195,19 @@ class ParseCopybookFeaturesSpec extends FunSuite with SimpleComparisonBase {
     val expectedLayout =
       """-------- FIELD LEVEL/NAME --------- --ATTRIBS--    FLD  START     END  LENGTH
         |
-        |1 RECORD                                              1      1     60     60
+        |1 RECORD                                              1      1    156    156
         |  5 FILLER_P1                                         2      1      1      1
         |  5 COMPANY_PREFIX                                    3      2      4      3
-        |  5 FILLER_P2                                         4      5      5      1
-        |  5 FILLER_P3                                         5      6      6      1
-        |  5 COMPANY_NAME                       r              6      7     15      9
-        |  5 ADDRESS                            r              7     16     45     30
-        |  5 FILL_FIELD                         r              8     46     52      7
-        |    10 FILLER_P6                                      9     46     50      5
-        |    10 FILLER_P7                                     10     51     52      2
-        |  5 CONTACT_PERSON                     R             11     46     52      7
-        |    10 FIRST_NAME                                    12     46     51      6
-        |  5 AMOUNT                                           13     53     60      8
+        |  5 FILLER_P2                                         4    101    101      1
+        |  5 FILLER_P3                                         5    102    102      1
+        |  5 COMPANY_NAME                       r              6    103    111      9
+        |  5 ADDRESS                            r              7    112    141     30
+        |  5 FILL_FIELD                         r              8    142    148      7
+        |    10 FILLER_P6                                      9    142    146      5
+        |    10 FILLER_P7                                     10    147    148      2
+        |  5 CONTACT_PERSON                     R             11    142    148      7
+        |    10 FIRST_NAME                                    12    142    147      6
+        |  5 AMOUNT                                           13    149    156      8
         |"""
         .stripMargin.replace("\r\n", "\n")
 
@@ -205,13 +221,13 @@ class ParseCopybookFeaturesSpec extends FunSuite with SimpleComparisonBase {
     val expectedLayout =
       """-------- FIELD LEVEL/NAME --------- --ATTRIBS--    FLD  START     END  LENGTH
         |
-        |1 RECORD                                              1      1     60     60
+        |1 RECORD                                              1      1    156    156
         |  5 COMPANY_PREFIX                                    2      2      4      3
-        |  5 COMPANY_NAME                       r              3      7     15      9
-        |  5 ADDRESS                            r              4     16     45     30
-        |  5 CONTACT_PERSON                     R              5     46     52      7
-        |    10 FIRST_NAME                                     6     46     51      6
-        |  5 AMOUNT                                            7     53     60      8
+        |  5 COMPANY_NAME                       r              3    103    111      9
+        |  5 ADDRESS                            r              4    112    141     30
+        |  5 CONTACT_PERSON                     R              5    142    148      7
+        |    10 FIRST_NAME                                     6    142    147      6
+        |  5 AMOUNT                                            7    149    156      8
         |"""
         .stripMargin.replace("\r\n", "\n")
 
