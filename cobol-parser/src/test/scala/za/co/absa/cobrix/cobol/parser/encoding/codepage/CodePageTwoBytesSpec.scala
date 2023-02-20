8f7a29084f9047ea67430308bdc5bf8d4984fb9c
Ruslan Iushchenko <yruslan@gmail.com>
2023-02-20 09:47:08 +0100

    #580 Add experimental two-byte code page CP300.

---
+++ cobol-parser/src/test/scala/za/co/absa/cobrix/cobol/parser/encoding/codepage/CodePageTwoBytesSpec.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.cobrix.cobol.parser.encoding.codepage
+
+import org.scalatest.funsuite.AnyFunSuite
+
+import scala.util.{Failure, Success, Try}
+
+class CodePageTwoBytesSpec extends AnyFunSuite {
+  test("Ensure codepage 'cp300' gives the associated CodePage") {
+    val codePage = CodePage.getCodePageByName("cp300")
+    assert(codePage.codePageShortName == "cp300")
+  }
+
+  test("Ensure 'ccsid300' decodes strings as expected") {
+    val bytes = Array[Byte](0x14, 0x10, 0x14, 0x11 ,0x14, 0x12)
+
+    val codePage = CodePage.getCodePageByName("cp300")
+
+    assert(codePage.convert(bytes) =="抜抛抗")
+  }
+}
