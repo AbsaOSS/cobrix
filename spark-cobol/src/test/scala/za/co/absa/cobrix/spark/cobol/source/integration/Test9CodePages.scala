c927bd486a33de91a9de99e3610d56be17631253
Ruslan Iushchenko <yruslan@gmail.com>
2024-01-16 09:40:06 +0100

    #263 Add support for CP500 EBCDIC codepage.

--- spark-cobol/src/test/scala/za/co/absa/cobrix/spark/cobol/source/integration/Test9CodePages.scala
+++ spark-cobol/src/test/scala/za/co/absa/cobrix/spark/cobol/source/integration/Test9CodePages.scala
@@ -77,9 +77,15 @@ class Test9CodePages extends AnyFunSuite with SparkTestBase {
         "string_trimming_policy" -> "none" :: Nil)
   }
 
-  test(s"Integration test on $exampleName code page CP273 extended") {
+  test(s"Integration test on $exampleName code page CP273") {
     runTest("_cp273",
       "ebcdic_code_page" -> "cp273" ::
+        "string_trimming_policy" -> "none" :: Nil)
+  }
+
+  test(s"Integration test on $exampleName code page CP500") {
+    runTest("_cp500",
+      "ebcdic_code_page" -> "cp500" ::
         "string_trimming_policy" -> "none" :: Nil)
   }
 
