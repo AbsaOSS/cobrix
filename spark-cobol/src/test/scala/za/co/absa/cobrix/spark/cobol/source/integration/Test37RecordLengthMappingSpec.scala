662b8040b5b2f7efdf0c07df20e90793e80ea4a6
Ruslan Iushchenko <yruslan@gmail.com>
2024-04-22 16:27:10 +0200

    #666 Add support for file offsets with record length fields.

--- spark-cobol/src/test/scala/za/co/absa/cobrix/spark/cobol/source/integration/Test37RecordLengthMappingSpec.scala
+++ spark-cobol/src/test/scala/za/co/absa/cobrix/spark/cobol/source/integration/Test37RecordLengthMappingSpec.scala
@@ -33,6 +33,11 @@ class Test37RecordLengthMappingSpec extends AnyWordSpec with SparkTestBase with 
     0xC2, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6,                     // record 1 'B123456'
     0xC3, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7).map(_.toByte) // record 2 'C1234567890'
 
+  val dataNumeric: Array[Byte] = Array(
+    0xF1, 0xF1, 0xF2, 0xF3,                                       // record 0 '1123'
+    0xF2, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6,                     // record 1 '1123456'
+    0xF3, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7).map(_.toByte) // record 2 '11234567890'
+
   val dataWithFileOffsets: Array[Byte] = Array(
     0x00,                                                         // header
     0xC1, 0xF1, 0xF2, 0xF3,                                       // record 0 'A123'
@@ -42,7 +47,7 @@ class Test37RecordLengthMappingSpec extends AnyWordSpec with SparkTestBase with 
 
   "data with record length mapping" should {
     "work for simple mappings" in {
-      withTempBinFile("rdw_test", ".tmp", dataSimple) { tempFile =>
+      withTempBinFile("record_length_mapping", ".tmp", dataSimple) { tempFile =>
         val expected = """{"SEG_ID":"A","TEXT":"123"},{"SEG_ID":"B","TEXT":"123456"},{"SEG_ID":"C","TEXT":"1234567"}"""
 
         val df = spark.read
@@ -61,8 +66,28 @@ class Test37RecordLengthMappingSpec extends AnyWordSpec with SparkTestBase with 
       }
     }
 
+    "work for numeric mappings" in {
+      withTempBinFile("record_length_mapping", ".tmp", dataNumeric) { tempFile =>
+        val expected = """{"SEG_ID":"1","TEXT":"123"},{"SEG_ID":"2","TEXT":"123456"},{"SEG_ID":"3","TEXT":"1234567"}"""
+
+        val df = spark.read
+          .format("cobol")
+          .option("copybook_contents", copybook)
+          .option("record_format", "F")
+          .option("record_length_field", "SEG-ID")
+          .option("input_split_records", "2")
+          .option("pedantic", "true")
+          .option("record_length_map", """{"1":4,"2":7,"3":8}""")
+          .load(tempFile)
+
+        val actual = df.orderBy("SEG_ID").toJSON.collect().mkString(",")
+
+        assert(actual == expected)
+      }
+    }
+
     "work for data with offsets" in {
-      withTempBinFile("rdw_test", ".tmp", dataSimple) { tempFile =>
+      withTempBinFile("record_length_mapping", ".tmp", dataWithFileOffsets) { tempFile =>
         val expected = """{"SEG_ID":"A","TEXT":"123"},{"SEG_ID":"B","TEXT":"123456"},{"SEG_ID":"C","TEXT":"1234567"}"""
 
         val df = spark.read
@@ -82,8 +107,30 @@ class Test37RecordLengthMappingSpec extends AnyWordSpec with SparkTestBase with 
       }
     }
 
+    "work for data with offsets and indexes" in {
+      withTempBinFile("record_length_mapping", ".tmp", dataWithFileOffsets) { tempFile =>
+        val expected = """{"SEG_ID":"A","TEXT":"123"},{"SEG_ID":"B","TEXT":"123456"},{"SEG_ID":"C","TEXT":"1234567"}"""
+
+        val df = spark.read
+          .format("cobol")
+          .option("copybook_contents", copybook)
+          .option("record_format", "F")
+          .option("record_length_field", "SEG-ID")
+          .option("file_start_offset", 1)
+          .option("file_end_offset", 2)
+          .option("input_split_records", "2")
+          .option("pedantic", "true")
+          .option("record_length_map", """{"A":4,"B":7,"C":8}""")
+          .load(tempFile)
+
+        val actual = df.orderBy("SEG_ID").toJSON.collect().mkString(",")
+
+        assert(actual == expected)
+      }
+    }
+
     "throw an exception for unknown mapping" in {
-      withTempBinFile("rdw_test", ".tmp", dataSimple) { tempFile =>
+      withTempBinFile("record_length_mapping", ".tmp", dataSimple) { tempFile =>
         val df = spark.read
           .format("cobol")
           .option("copybook_contents", copybook)
@@ -98,6 +145,25 @@ class Test37RecordLengthMappingSpec extends AnyWordSpec with SparkTestBase with 
         }
 
         assert(ex.getMessage.contains("Record length value 'C' is not mapped to a record length"))
+      }
+    }
+
+    "throw an exception for null fields" in {
+      withTempBinFile("rdw_test", ".tmp", dataWithFileOffsets) { tempFile =>
+        val df = spark.read
+          .format("cobol")
+          .option("copybook_contents", copybook)
+          .option("record_format", "F")
+          .option("record_length_field", "SEG-ID")
+          .option("pedantic", "true")
+          .option("record_length_map", """{"A":4,"B":7,"C":8}""")
+          .load(tempFile)
+
+        val ex = intercept[SparkException] {
+          df.count()
+        }
+
+        assert(ex.getMessage.contains("Null encountered as a record length field (offset: 1, raw value: 00)"))
       }
     }
   }
