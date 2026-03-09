bfa90d68c715761a1424fbdfa0c2a2e9d3a64521
Ruslan Iushchenko <yruslan@gmail.com>
2026-03-09 10:28:43 +0100

    #797 Implemented PR suggestions and improved test coverage slightly.

--- spark-cobol/src/main/scala/za/co/absa/cobrix/spark/cobol/source/parameters/CobolParametersValidator.scala
+++ spark-cobol/src/main/scala/za/co/absa/cobrix/spark/cobol/source/parameters/CobolParametersValidator.scala
@@ -19,6 +19,7 @@ package za.co.absa.cobrix.spark.cobol.source.parameters
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import org.apache.spark.SparkConf
+import org.slf4j.LoggerFactory
 import za.co.absa.cobrix.cobol.parser.recordformats.RecordFormat
 import za.co.absa.cobrix.cobol.reader.parameters.CobolParametersParser._
 import za.co.absa.cobrix.cobol.reader.parameters.{CobolParameters, ReaderParameters}
@@ -32,7 +33,18 @@ import scala.collection.mutable.ListBuffer
   * This class provides methods for checking the Spark job options after parsed.
   */
 object CobolParametersValidator {
+  private val log = LoggerFactory.getLogger(this.getClass)
 
+  /**
+    * Validates the provided COBOL processing parameters and throws an exception
+    * if any inconsistency or missing configuration is detected.
+    *
+    * This method checks only if the copybook is defined and not ambiguous.
+    *
+    * @param params the COBOL processing parameters to be validated
+    * @return this method does not return any value. It throws an IllegalArgumentException if
+    *         the validation fails.
+    */
   def checkSanity(params: CobolParameters): Unit = {
     if (params.sourcePaths.isEmpty) {
       throw new IllegalArgumentException("Data source path must be specified.")
@@ -43,12 +55,40 @@ object CobolParametersValidator {
     }
   }
 
+  /**
+    * Validates the provided Spark and Hadoop configuration objects and throws an exception
+    * if any inconsistency or issue is detected in the configurations.
+    *
+    * The method extracts essential parameters from the Spark configuration, validates these
+    * parameters alongside the Hadoop configuration object, and ensures that all required
+    * settings are present and correct. Conflicting or missing configurations are identified
+    * and result in exceptions being thrown.
+    *
+    * @param sparkConf  the Spark configuration object containing parameters necessary for
+    *                   this validation step.
+    * @param hadoopConf the Hadoop configuration object used to perform validation tasks
+    *                   related to Hadoop filesystem operations.
+    * @return this method does not return any value. It throws an exception if any validation fails.
+    */
   def validateOrThrow(sparkConf: SparkConf, hadoopConf: Configuration): Unit = {
     val parameters = Map[String, String](PARAM_COPYBOOK_PATH -> sparkConf.get(PARAM_COPYBOOK_PATH), PARAM_SOURCE_PATH -> sparkConf.get
     (PARAM_SOURCE_PATH))
     validateOrThrow(parameters, hadoopConf)
   }
 
+  /**
+    * Validates the provided parameters for processing COBOL data and throws an exception
+    * if any inconsistency or issue is detected in the configurations.
+    *
+    * The method checks for the presence and validity of parameters required for data input
+    * and ensures that conflicting or missing configurations are handled correctly. It also
+    * verifies the validity and accessibility of copybook files when specified.
+    *
+    * @param parameters a map of configuration options to be validated.
+    * @param hadoopConf the Hadoop configuration object used to support filesystem operations
+    *                   when validating copybook paths in distributed storage systems.
+    * @return this method does not return any value. It throws an exception if any validation fails.
+    */
   def validateOrThrow(parameters: Map[String, String], hadoopConf: Configuration): Unit = {
     val copyBookContents = parameters.get(PARAM_COPYBOOK_CONTENTS)
     val copyBookPathFileName = parameters.get(PARAM_COPYBOOK_PATH)
@@ -116,12 +156,25 @@ object CobolParametersValidator {
     }
   }
 
+  /**
+    * Validates the parameters provided for writing operations in COBOL data processing.
+    * Verifies that the specified configurations are compatible with the writer and throws
+    * an exception in case of violations.
+    *
+    * @param readerParameters the configuration containing options to be validated for writing operations.
+    * @return this method does not return any value but throws an IllegalArgumentException if the validation fails.
+    */
   def validateParametersForWriting(readerParameters: ReaderParameters): Unit = {
     val issues = new ListBuffer[String]
 
     if (readerParameters.recordFormat != RecordFormat.FixedLength && readerParameters.recordFormat != RecordFormat.VariableLength) {
       issues += s"Only '${RecordFormat.FixedLength}' and '${RecordFormat.VariableLength}' values for 'record_format' are supported for writing, " +
         s"provided value: '${readerParameters.recordFormat}'"
+    }
+
+    if (readerParameters.variableSizeOccurs &&
+      readerParameters.recordFormat == RecordFormat.FixedLength) {
+      log.warn("Option 'variable_size_occurs=true' is used with 'record_format=F' which means records can have variable length. It is highly recommended to use 'record_format=V' instead.")
     }
 
     if (readerParameters.occursMappings.nonEmpty) {
